04323ac22e178a3f464919d8c2034bdde6ca8f3c
dhukke <dhukke@gmail.com>
2021-03-22 20:08:13 -0300

    added query

--- BackgroundRabbit/ConsumerHandler.cs
+++ BackgroundRabbit/ConsumerHandler.cs
@@ -24,13 +24,34 @@ namespace BackgroundRabbit
             )
             {
                 connection.Open();
+
                 var sql = "INSERT INTO Messages(Id, Content) VALUES (@id, @content)";
                 var command = new SqlCommand(sql, connection);
-                //Adicionando o valor das textBox nos parametros do comando
+
                 command.Parameters.Add(new SqlParameter("@id", content.Id));
                 command.Parameters.Add(new SqlParameter("@content", content.Text));
 
                 command.ExecuteNonQuery();
+
+                var sqlGet = "select top(1) id, content from Messages where id = @id";
+
+                var commandGet = new SqlCommand(sqlGet, connection);
+                commandGet.Parameters.AddWithValue("@id", content.Id);
+
+                var reader = commandGet.ExecuteReader();
+
+                try
+                {
+                    while (reader.Read())
+                    {
+                        _logger.LogInformation("{0}, {1}", reader["id"], reader["content"]);
+                    }
+                }
+                finally
+                {
+                    // Always call Close when done reading.
+                    reader.Close();
+                }
 
                 //fecha a conexao
                 connection.Close();
